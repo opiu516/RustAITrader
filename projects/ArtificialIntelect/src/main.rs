de219ec28e758308e41c9be03619e399e19b1054
Martin Kostov <martin.kostov@lights.digital>
2023-10-05 21:20:41 +0300

    Created random samples on run

--- projects/ArtificialIntelect/src/main.rs
+++ projects/ArtificialIntelect/src/main.rs
@@ -2,6 +2,8 @@ mod layer;
 mod node;
 mod neural_network;
 
+use rand::Rng; 
+
 fn simple_activation_function(number:f32) -> f32{
     let mut res:f32 = 0.0;
     if number > 0.0 {
@@ -11,7 +13,16 @@ fn simple_activation_function(number:f32) -> f32{
 }
 
 fn main() {
-    let network = neural_network::NeuralNetwork::new(7,vec![5,5,4,1],simple_activation_function,|vec: Vec<f32>| -> Vec<f32> { vec });
-    network.display();
-    println!("res = {:?}",network.calculate(vec![2.0,1.0,11.0,5.0,2.0,3.5,6.0]));
+    let input = rand::thread_rng().gen_range(1..10);
+    let hidden_layers = rand::thread_rng().gen_range(1..50);
+    let mut hidden_layers_vector : Vec<u32> = Vec::new();
+    let mut inputs : Vec<f32> = Vec::new();
+    for _i in 0..hidden_layers{
+        hidden_layers_vector.push(rand::thread_rng().gen_range(5..10));
+    }
+    for _i in 0..input{
+        inputs.push((rand::thread_rng().gen_range(-100..100) as f32)/(10 as f32));
+    }
+    let network = neural_network::NeuralNetwork::new(input,hidden_layers_vector,simple_activation_function,|vec: Vec<f32>| -> Vec<f32> { vec });
+    println!("res = {:?}",network.calculate(inputs));
 }
